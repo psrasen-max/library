1525907ed36df17da3f96d928f34b5b3007fcb6b
Murilo Gonçalves <psrasen@gmail.com>
2026-01-06 16:47:46 -0300

    alterações no idioma e tabela users na migration

--- database/migrations/2025_12_31_144458_create_users_table.php
+++ database/migrations/2025_12_31_144458_create_users_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30);
+            $table->string('email', 50)->unique();
+            $table->string('phone', 11)->unique();
+            $table->string('password');
+            $table->string('adress');
+            $table->string('cep', 8);
+            $table->integer('lat');
+            $table->integer('long');
+            $table->boolean('is_admin');
             $table->timestamps();
         });
     }
