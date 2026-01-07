8e60f124c39f1c1e7acf579c17e88d3e4443c7fa
Murilo Gonçalves <psrasen@gmail.com>
2026-01-07 13:01:01 -0300

    criação (mcfs)

---
+++ database/seeders/UserEntrieSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserEntrieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
